c538741900348af3de3a09d440a04f43d5e3eef5
vincent <vincent.arteaga@gmail.com>
2021-06-14 09:57:16 -0700

    assignment complete

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,43 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+class YouTubeDebugger extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {   
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            } }
+    }
+
+    handleBitClick = (e) => {
+        this.setState({
+            settings: {
+            ...this.state.settings, bitrate: 12 }
+            })
+    }
+    handleResClick = (e) => {
+        this.setState({
+            settings: {
+            ...this.state.settings, video: {
+                ...this.state.settings.video,
+                resolution: '720p' }
+        }})
+    }
+
+    render() { 
+        return ( 
+            <React.Fragment>
+            <button onClick={this.handleBitClick} className='bitrate' >bitrate: {this.state.settings.bitrate}</button>
+            <button onClick={this.handleResClick} className='resolution'>resolution: {this.state.settings.video.resolution}</button> 
+            </React.Fragment>
+            );
+    }
+}
+ 
+export default YouTubeDebugger;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import YouTubeDebugger from './components/YouTubeDebugger';
 ReactDOM.render(
   <div>
     <DigitalClicker />
-    <YouTubeDebugger />
+    <p><YouTubeDebugger /></p>
   </div>,
   document.getElementById('global')
 );
